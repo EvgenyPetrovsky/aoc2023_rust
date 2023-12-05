222a00f8d5bd32ccb24128bdec148681a8bdfe29
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2023-12-05 22:32:17 +0100

    fix range start values after mapping

--- src/solution/day_05.rs
+++ src/solution/day_05.rs
@@ -6,7 +6,7 @@ use crate::Part;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Rng {start: u64, len: u64}
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Mapping {dest_start: u64, src_rng: Rng}
 
 
@@ -32,7 +32,7 @@ impl DaySolution {
                 })
                 .collect(),
             Part::Two =>
-                Regex::new(r#"\d+ \d+"#)
+                Regex::new(r#"(\d+) (\d+)"#)
                 .unwrap()
                 .captures_iter(line)
                 .map(|c| {
@@ -143,12 +143,12 @@ impl DaySolution {
                 let dest = m.dest_start;
                 // {seed_rng} to the left  from [mapping]: __{____}_[____]__
                 // {seed_rng} to the right from [mapping]: __[____]_{____}__
-                if s_end <= m_beg || s_beg > m_end {
+                if s_end <= m_beg || m_end <= s_beg {
                     None
                 }
                 // right part {seed_rng} inside [mapping]: __{__[**}__]__
                 else if s_beg <= m_beg && m_beg < s_end && s_end <= m_end {
-                    Some(Rng { start: dest + (m_beg - s_beg), len: s_end - m_beg }) }
+                    Some(Rng { start: dest, len: s_end - m_beg }) }
                 // whole      {seed_rng} inside [mapping]: __[__{**}__]__
                 else if m_beg <= s_beg && s_end <= m_end {
                     Some(Rng { start: dest + (s_beg - m_beg), len: s_end - s_beg })}
@@ -157,14 +157,15 @@ impl DaySolution {
                     Some(Rng { start: dest + (s_beg - m_beg), len: m_end - s_beg }) }
                 // {seed_rng} fully includes [mapping]:    __{__[**]__}__
                 else if s_beg <= m_beg && m_end <= s_end {
-                    Some(Rng { start: dest + (m_beg - s_beg), len: m_end - m_beg }) }
+                    Some(Rng { start: dest, len: m_end - m_beg }) }
                 // :-O unknown case
                 else {panic!("unknown case! seed: {:?}, mapping: {:?}", seed_rng, m)}
             })
             .collect()
 
     }
-    fn map_though_all(seed_rng: Rng, mappings_set: &HashMap<String, Vec<Mapping>>) -> u64 {
+
+    fn map_though_all(seed_rng: Rng, mappings_set: &HashMap<String, Vec<Mapping>>) -> Vec<Rng> {
         let init = vec![seed_rng];
         let mapping_cats = ["seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location",];
         mapping_cats
@@ -174,10 +175,6 @@ impl DaySolution {
             .flat_map(|rng| Self::map_once(rng, &mappings_set[x]))
             .collect()
         })
-        .iter()
-        .map(|r| r.start)
-        .min()
-        .unwrap()
     }
 
 }
@@ -202,16 +199,24 @@ impl super::Solution for DaySolution {
         problem.seeds
         .iter()
         .map(|&r| DaySolution::map_though_all(r, &problem.maps))
+        .flatten()
+        .map(|r| r.start)
         .min()
     }
 
-    fn solve_part_2(_problem: Self::Problem) -> Self::Answer {
+    fn solve_part_2(problem: Self::Problem) -> Self::Answer {
         /*
         Okay, we need to update solution so it operates ranges.
         every time when range passed into mapping list of ranges is returned: (num_rng, Mapping) -> [num_rng]
         each range from returned list is mapped trhough next mapping
+        the same logic is used to solve part 1 and 2, but inputs are slightly different: see parse_input_part_2
          */
-        None
+        problem.seeds
+        .iter()
+        .map(|&r| DaySolution::map_though_all(r, &problem.maps))
+        .flatten()
+        .map(|r| r.start)
+        .min()
     }
 
     fn show_answer(answer: Self::Answer) -> String {
@@ -221,3 +226,51 @@ impl super::Solution for DaySolution {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    //use super::{Room, DaySolution};
+
+    use super::{DaySolution, Mapping, Rng};
+
+    #[test]
+    fn parse_mapping_lines() {
+        let line = "50 98 2\n52 50 48";
+        //BallSet { red: 4, green: 0, blue: 3 },
+        //BallSet { red: 1, green: 2, blue: 6 },
+        //BallSet { red: 0, green: 2, blue: 0 }
+        assert_eq!(
+            {
+                let mut l = DaySolution::parse_mapping_lines(line);
+                l.sort_by(|a, b| a.src_rng.start.cmp(&b.src_rng.start));
+                l
+            },
+            vec![
+                Mapping{ dest_start:   0, src_rng: Rng{ start:   0, len: 50 }},
+                Mapping{ dest_start:  52, src_rng: Rng{ start:  50, len: 48 }},
+                Mapping{ dest_start:  50, src_rng: Rng{ start:  98, len:  2 }},
+                Mapping{ dest_start: 100, src_rng: Rng{ start: 100, len: u64::MAX - 100 }},
+            ]
+        )
+    }
+    #[test]
+    fn map_once() {
+        let line = "50 98 2\n52 50 48";
+        assert_eq!(
+            {
+                let mut ms = DaySolution::parse_mapping_lines(line);
+                ms.sort_by(|a, b| a.src_rng.start.cmp(&b.src_rng.start));
+                DaySolution::map_once(&Rng{ start: 95, len: 10 }, &ms)
+            },
+            vec![
+                Rng{ start: 52 + (95 - 50), len: 3 }, //  95 ..  98
+                Rng{ start: 50 + (98 - 98), len: 2 }, //  98 .. 100
+                Rng{ start:100 + (100-100), len: 5 }, // 100 .. 105
+            ]
+        )
+    }
+
+}
+
+//
