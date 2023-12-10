bec21342081c08a87b9f47f8a1a2ac7847e5367e
Evgeny Petrovsky <evgeny.petrovsky@ing.com>
2023-12-10 10:36:30 +0100

    day 10 game logic

--- src/solution/day_10.rs
+++ src/solution/day_10.rs
@@ -1,28 +1,77 @@
 type P = ();
 
 struct Position{r: usize, c: usize}
+#[derive(Debug, PartialEq, Eq)]
 enum Segment {NS, EW, NE, NW, SW, SE, S, O}
+#[derive(Debug, PartialEq, Eq)]
 enum Direction {N, E, S, W}
 struct State {pos: Position, dir: Direction}
-type Map = Vec<Vec<Segment>>;
+struct Map { max_pos: Position, content: Vec<Vec<Segment>> }
 pub struct DaySolution(P);
 
 impl DaySolution {
-    fn parse_byte(b: Byte) -> Segment {
-        match b {
-            b'|' => NS,
-            b'-' => EW,
-            b'L' => NE,
-            b'J' => NW,
-            b'F' => SE,
-            b'7' => SW,
-            b'.' => O,
-            b'S' => S,
-            _ => panic!("could't parse '{}'", b.make_ascii_uppercase()),
+    fn parse_byte(c: char) -> Segment {
+
+        match c as u8 {
+            b'|' => Segment::NS,
+            b'-' => Segment::EW,
+            b'L' => Segment::NE,
+            b'J' => Segment::NW,
+            b'F' => Segment::SE,
+            b'7' => Segment::SW,
+            b'.' => Segment::O,
+            b'S' => Segment::S,
+            _ => panic!("could't parse '{}'", c),
         }
     }
-    fn locate_start() -> Position {
-        unimplemented!()
+    fn locate_start(map: &Map) -> Position {
+        let cardinality = map.max_pos.r * map.max_pos.c;
+        let c = map.max_pos.c;
+        (0..cardinality)
+        .map(|i| {(i/c, i%c)})
+        .filter_map(|(r,c)| {
+            if map.content[r][c] == Segment::S {Some(Position {r, c})} else {None}
+        })
+        .nth(0)
+        .unwrap()
+    }
+    fn init_state(map: &Map) -> State {
+        let pos = Self::locate_start(map);
+        // we assume that we always star facing south (based on test and real input)
+        State { dir: Direction::S, pos }
+    }
+    fn move_once(map: &Map, state: State) -> State{
+        let (r0,c0) = (state.pos.r, state.pos.c);
+        let d0 = &state.dir;
+        let (r1, c1) = match d0 {
+            Direction::N => (r0+0, c0-1),
+            Direction::E => (r0+1, c0+0),
+            Direction::S => (r0+0, c0+1),
+            Direction::W => (r0-1, c0+0),
+        };
+        let s1 = &map.content[r1][c1];
+        let d1 = match (d0, s1) {
+            (Direction::N, Segment::NS) => Direction::N,
+            (Direction::N, Segment::SE) => Direction::E,
+            (Direction::N, Segment::SW) => Direction::W,
+
+            (Direction::E, Segment::EW) => Direction::E,
+            (Direction::E, Segment::NW) => Direction::N,
+            (Direction::E, Segment::SW) => Direction::S,
+
+            (Direction::S, Segment::NS) => Direction::S,
+            (Direction::S, Segment::NE) => Direction::E,
+            (Direction::S, Segment::NW) => Direction::W,
+
+            (Direction::W, Segment::EW) => Direction::W,
+            (Direction::W, Segment::NE) => Direction::N,
+            (Direction::W, Segment::SE) => Direction::S,
+
+            _ => panic!("Couldn't recognize new direction based on {:?} and {:?}", d0, s1),
+        };
+
+        State{ pos: Position { r: r1, c: c1 }, dir: d1 }
+
     }
   //fn move()
 }
