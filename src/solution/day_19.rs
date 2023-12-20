18128d62b4291d42468925ba2a13f6890147b911
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2023-12-20 22:53:43 +0100

    some failing code for day 19

--- src/solution/day_19.rs
+++ src/solution/day_19.rs
@@ -3,23 +3,31 @@ use regex::Regex;
 
 pub struct Part {x: u32, m: u32, a: u32, s: u32}
 
+#[derive(Debug, Clone)]
 pub struct Workflow { name: String, rules: Vec<Rule> }
 
+#[derive(Debug, Clone)]
 enum Rule { Condition(Condition), Decision(Decision) }
 
+#[derive(Debug, Clone)]
 struct Condition { par: char, cmp: Comparison, val: u32, dec: Decision }
 
+#[derive(Debug, Clone)]
 enum Decision { Accept, Reject, SendTo (String) }
 
+#[derive(Debug, Clone)]
 enum Comparison { Lt, Gt }
 
-
-type P = (HashMap<String, Workflow>, Vec<Part>);
+pub struct P {
+    start: String,
+    flows: HashMap<String, Workflow>,
+    parts: Vec<Part>
+}
 
 impl DaySolution {
 
     fn parse_one_rule (line: &str) -> Rule {
-        let re_condition = Regex::new(r#"^([xmas])([\<\>])(\d+):([AR]|[a-z]+)$"#).unwrap();
+        let re_condition = Regex::new(r#"^([xmas])([<>])(\d+):([AR]|[a-z]+)$"#).unwrap();
         let re_decision  = Regex::new(r#"^([AR]|[a-z]+)$"#).unwrap();
 
         fn match_decision(dec: &str) -> Decision {
@@ -32,22 +40,33 @@ impl DaySolution {
         let condition =
             re_condition
             .captures(line)
-            .map(|c| {
-            let (_, [par, cmp, val, dec]) = c.extract();
-                Condition {
-                    par: par.chars().nth(0).unwrap(),
-                    cmp: if cmp == "<" {Comparison::Lt} else {Comparison::Gt},
-                    val: val.parse::<u32>().unwrap(),
-                    dec: match_decision(dec),
-                }
-        });
+            .map(|c| match c.get(0) {
+                Some(_) => {
+                    let (_, [par, cmp, val, dec]) = c.extract();
+                    let c = Condition {
+                        par: par.chars().nth(0).unwrap(),
+                        cmp: if cmp == "<" {Comparison::Lt} else {Comparison::Gt},
+                        val: val.parse::<u32>().unwrap(),
+                        dec: match_decision(dec),
+                    };
+                    Some(c)
+                },
+                None => None
+            })
+            .unwrap();
         let decision =
             re_decision
             .captures(line)
-            .map(|c| {
-                let (_, [dec]) = c.extract();
-                match_decision(dec)
-            });
+            .map(|c| { match c.get(0) {
+                Some(_) => {
+                    let (_, [dec]) = c.extract();
+                    Some(match_decision(dec))
+                },
+                None => None
+
+            }
+            })
+            .unwrap();
 
         if condition.is_some() {
             Rule::Condition(condition.unwrap())
@@ -57,8 +76,8 @@ impl DaySolution {
 
     }
     fn parse_one_workflow(line: &str) -> Workflow {
-        let main_re = Regex::new(r#"(\w+)\{([a-zAR\<\>\:,]+)\}"#).unwrap();
-        let rules_re = Regex::new(r#"[0-9a-zAR\<\>\:]+"#).unwrap();
+        let main_re = Regex::new(r#"(\w+)\{([0-9a-zAR<>:,]+)\}"#).unwrap();
+        let rules_re = Regex::new(r#"[0-9a-zAR<>:]+"#).unwrap();
         let (name, rules_str) =
         main_re.captures(line).map(|c| {
             let(_, [name, rules]) = c.extract();
@@ -87,6 +106,48 @@ impl DaySolution {
         })
         .unwrap()
     }
+
+    fn classify_one_part(part: &Part, flow: &String, flows: &HashMap<String, Workflow>) -> Decision {
+
+        let rules = flows.get(flow).unwrap().clone().rules;
+
+        let opt_decision: Option<Decision> = rules
+            .iter()
+            .fold(None, |z, rule| {
+
+                let new_decision =
+                    match rule {
+                        Rule::Condition(c) => {
+                            let lhs = match c.par {
+                                'x' => part.x,
+                                'm' => part.m,
+                                'a' => part.a,
+                                's' => part.s,
+                                _ => unreachable!("pasrt name is not in XMAS list (condition:{:?})", &c)
+                            };
+                            match c.cmp {
+                                Comparison::Gt => if lhs > c.val {Some(c.dec.clone())} else {None},
+                                Comparison::Lt => if lhs < c.val {Some(c.dec.clone())} else {None},
+                            }
+                        }
+                        Rule::Decision(d) => Some(d.clone()),
+                    };
+                    match z {
+                        Some(d) => Some(d),
+                        None => new_decision,
+                    }
+            });
+        match opt_decision {
+            Some(Decision::SendTo(new_flow)) => Self::classify_one_part(part, &new_flow, flows),
+            Some(Decision::Accept) => Decision::Accept,
+            Some(Decision::Reject) => Decision::Reject,
+            None => Decision::Accept,
+
+        }
+
+
+    }
+
 }
 
 pub struct DaySolution(P);
@@ -95,19 +156,52 @@ impl super::Solution for DaySolution {
 
     const DAY_NUMBER: u8 = 19;
 
-    type Answer = Option<i32>;
+    type Answer = Option<usize>;
     type Problem = P;
 
-    fn parse_input_part_1(_text_input: String) -> Self::Problem {
-        unimplemented!();
+    fn parse_input_part_1(text_input: String) -> Self::Problem {
+        let v: Vec<&str> = text_input.split("\n\n").collect();
+        let flows_str = v[0];
+        let parts_str = v[1];
+        let parts: Vec<Part> = parts_str
+            .lines()
+            .map(|line| DaySolution::parse_one_part(line))
+            .collect();
+        let flows: HashMap<String, Workflow> = flows_str
+            .lines()
+            .map(|line| DaySolution::parse_one_workflow(line))
+            .map(|wfw| (wfw.name.clone(), wfw))
+            .collect()
+            ;
+        let start = flows_str.
+            lines().
+            take(1).
+            map(|line| {
+                let one_flow = DaySolution::parse_one_workflow(line);
+                one_flow.name
+            })
+            .nth(0)
+            .unwrap()
+            ;
+        P { start, flows, parts }
     }
 
-    fn parse_input_part_2(_text_input: String) -> Self::Problem {
-        Self::parse_input_part_1(_text_input)
+    fn parse_input_part_2(text_input: String) -> Self::Problem {
+        Self::parse_input_part_1(text_input)
     }
 
-    fn solve_part_1(_problem: Self::Problem) -> Self::Answer {
-        None
+    fn solve_part_1(problem: Self::Problem) -> Self::Answer {
+        let Self::Problem { start, parts, flows } = problem;
+        let answer =
+            parts
+            .iter()
+            .map(|part| DaySolution::classify_one_part(part, &start, &flows))
+            .filter(|decision| match decision {
+                Decision::Accept => true,
+                _ => false
+            })
+            .count();
+        Some(answer)
     }
 
     fn solve_part_2(_problem: Self::Problem) -> Self::Answer {
